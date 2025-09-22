b17a1b93d7b0709a97c1f68243ab4c312fa84d67
Jet@D:coder <2302543@ub.edu.ph>
2025-09-22 11:57:02 +0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,17 @@ import './App.css'
 
 function App() {
    const myElement = <h5>Hello World</h5> 
-
+   const myElement2 =<h5>The result of 5+5 is {5+5}</h5>
+   const name = "John Doe"
+   const age=25
   return (
     <>
     {myElement}
+    {myElement2}
+    <h1>Hello, {name}</h1>
+    <h5>you are {age} years old</h5>
     </>
+    
   )
 }
 
