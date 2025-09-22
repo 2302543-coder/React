3be970c5965353d7dff88923e9f05f93c2d0ec46
Jet@D:coder <2302543@ub.edu.ph>
2025-09-22 11:44:21 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+   const myElement = <h5>Hello World</h5> 
+
+  return (
+    <>
+    {myElement}
+    </>
+  )
+}
+
+export default App
